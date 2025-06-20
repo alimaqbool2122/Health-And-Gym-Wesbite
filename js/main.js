6d28988ae9c08a755f9b9a738f95b1ce7893c8a2
= <=>
2025-06-20 10:39:48 +0500

    initial

---
+++ js/main.js
@@ -0,0 +1,52 @@
+"use strict";
+
+
+    // preloader.
+
+
+
+	// Header Animation.
+
+
+
+
+	// For Navigation Menu.
+
+
+
+    // isotop
+
+
+    //for menu active class
+
+
+
+
+    // accordion variables
+
+
+
+    // Product-active
+
+
+
+    
+    /*----- cart-plus-minus-button -----*/
+
+
+
+    // DEALS TAB
+
+
+
+
+    // For brand slider
+   
+
+
+    // scrollToTop
+   
+
+
+
+    // One Page Nav
